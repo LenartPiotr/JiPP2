64a50b468e36d12c13fd8c43fa166a7198e4a740
LenartPiot <plenart13@gmail.com>
2021-10-11 16:13:45 +0200

    Utworzone pierwsze zadanie

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+void ZadanieTablice(){
+    // Tworzenie tablicy dwuwymiarowej
+    int ** tab = new int*[10];
+    for(int i=0; i<10; i++){
+        tab[i] = new int[10];
+    }
+    // Tablica utworzona
+    // Usuwanie
+    for(int i=0; i<10; i++) delete tab[i];
+    delete tab;
+}
+
+void ZadanieKalkulator(int argc, char *argv[]){
+    for(int i = 0; i < argc ; ++i) {
+        cout << argv[i] <<endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    ZadanieTablice();
+    ZadanieKalkulator(argc, argv);
     return 0;
 }
