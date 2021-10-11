77bf2b5edb2fe09e617f5addd3c6988dfda22e28
LenartPiot <plenart13@gmail.com>
2021-10-11 15:46:23 +0200

    Add other files

---
+++ cmake-build-debug/CMakeFiles/Lab02x.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab02x.dir/main.cpp.obj"
+  "Lab02x.exe"
+  "Lab02x.exe.manifest"
+  "Lab02x.lib"
+  "Lab02x.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab02x.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Lab02x.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lab02x.
+# This may be replaced when dependencies are built.
