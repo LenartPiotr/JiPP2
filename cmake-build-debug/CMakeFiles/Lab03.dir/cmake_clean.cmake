6ac12ac44421451953955911bdc6f44e4ba9b5bb
LenartPiot <plenart13@gmail.com>
2021-10-18 15:07:15 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/Lab03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab03.dir/main.cpp.obj"
+  "Lab03.exe"
+  "Lab03.exe.manifest"
+  "Lab03.lib"
+  "Lab03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
