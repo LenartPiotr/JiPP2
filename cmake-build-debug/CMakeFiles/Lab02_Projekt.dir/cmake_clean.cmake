d8b26d72407796d6e05d37c8a1d0bd89ae23862b
LenartPiot <plenart13@gmail.com>
2021-10-22 15:10:52 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/Lab02_Projekt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab02_Projekt.dir/main.cpp.obj"
+  "Lab02_Projekt.exe"
+  "Lab02_Projekt.exe.manifest"
+  "Lab02_Projekt.lib"
+  "Lab02_Projekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab02_Projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
