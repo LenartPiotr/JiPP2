3a278f561b6e02526cfce165d5301204c8f8e8c1
LenartPiot <plenart13@gmail.com>
2021-10-25 14:54:09 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/Lab04.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab04.dir/main.cpp.obj"
+  "Lab04.exe"
+  "Lab04.exe.manifest"
+  "Lab04.lib"
+  "Lab04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
