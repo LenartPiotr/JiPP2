51d034cb4431f19e37dd696cc29c5bbc09b7460e
LenartPiot <plenart13@gmail.com>
2021-11-22 15:22:22 +0100

    Zadania

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Figure{
+protected:
+    string nazwa;
+    string kolor;
+public:
+    virtual double getArea() = 0;
+    virtual double getPerimeter() = 0;
+};
+
+class Circle : public Figure{
+    double r;
+public:
+    Circle(double r):r(r){}
+    virtual double getArea(){
+        return M_PI*r*r;
+    }
+    virtual double getPerimeter(){
+        return 2*M_PI*r;
+    }
+};
+
+class Rectangle : public Figure{
+    double a,b;
+public:
+    Rectangle(double a,double b):a(a),b(b){}
+    virtual double getArea(){
+        return a*b;
+    }
+    virtual double getPerimeter(){
+        return 2*a+2*b;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Figure *circle = new Circle(5);
+    Figure *rectangle = new Rectangle(5, 18);
+
+    cout << "Circle area: " << circle->getArea() << endl;
+    cout << "Rectangle area: " << rectangle->getArea() << endl;
     return 0;
 }
