04b36ff9276d3ee7fefa50c0e3db7892ed68b37d
Emma Paris <48452012+piotr3pl@users.noreply.github.com>
2022-01-03 16:32:15 +0100

    działa

--- src/csv_CSVReader.cpp
+++ src/csv_CSVReader.cpp
@@ -9,24 +9,40 @@ CSVData CSVReader::read(char *fileName) {
     char buffer[1024];
     int index = 0;
     char sign;
+    bool inQuotation = false;
+    bool prevWasQuoteMark = false;
     if(file.is_open()){
         while(!file.eof()){
             file.get(sign);
             if(file.eof()) break;
             if(sign == ','){
-                buffer[index] = '\0';
-                string s(buffer);
-                data.addData(currentRow, s);
-                index = 0;
+                if(inQuotation){
+                    buffer[index++] = sign;
+                }else{
+                    buffer[index] = '\0';
+                    string s(buffer);
+                    data.addData(currentRow, s);
+                    index = 0;
+                }
+                prevWasQuoteMark = false;
             }else if(sign == '\n') {
+                if(inQuotation) throw CSVException("wrong file format");
                 buffer[index] = '\0';
                 index = 0;
                 string s(buffer);
                 data.addData(currentRow, s);
                 data.addRow();
                 currentRow++;
+                prevWasQuoteMark = false;
+            }else if(sign == '"'){
+                if(prevWasQuoteMark){
+                    buffer[index++] = '"';
+                }
+                prevWasQuoteMark = !prevWasQuoteMark;
+                inQuotation = !inQuotation;
             }else{
                 buffer[index++] = sign;
+                prevWasQuoteMark = false;
             }
         }
         buffer[index] = '\0';
